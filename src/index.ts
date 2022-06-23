b52126b0f7072640958ba3154e8ba5f567d39ae2
k.jeong <kwanwoo.jeong@gmail.com>
2022-06-24 03:19:41 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { getInput, setFailed } from '@actions/core';
 import { context, getOctokit } from '@actions/github';
 
+const ISSUE_NUMBER_REGEX = /[A-Z]{2,}-\d+/;
+
 (async () => {
   try {
     const githubToken = getInput('githubToken', { required: true });
@@ -11,7 +13,7 @@ import { context, getOctokit } from '@actions/github';
       pull_number: Number(context.payload.pull_request?.number),
     };
     const commits = await octokit.rest.pulls.listCommits({ ...payload, per_page: 100 });
-    console.log(commits);
+    console.log(commits.data.map(({ commit }) => ISSUE_NUMBER_REGEX.exec(commit.message)?.[0]));
   } catch (error: any) {
     console.table(error);
     setFailed(error?.errorMessages);
