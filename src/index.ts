685cca3b0d6f63f5ca890f7053f1e63a4d5dded1
k.jeong <kwanwoo.jeong@gmail.com>
2022-06-24 03:34:26 +0900

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { getInput, setFailed } from '@actions/core';
+import { getInput, setFailed, setOutput } from '@actions/core';
 import { context, getOctokit } from '@actions/github';
 
 const ISSUE_NUMBER_REGEX = /[A-Z]{2,}-\d+/;
@@ -12,8 +12,20 @@ const ISSUE_NUMBER_REGEX = /[A-Z]{2,}-\d+/;
       repo: context.repo.repo,
       pull_number: Number(context.payload.pull_request?.number),
     };
-    const commits = await octokit.rest.pulls.listCommits({ ...payload, per_page: 100 });
-    console.log(commits.data.map(({ commit }) => ISSUE_NUMBER_REGEX.exec(commit.message)?.[0]));
+
+    let shouldFetchMore = true;
+    let issueNumbers: string[] = [];
+    while (shouldFetchMore) {
+      const res = await octokit.rest.pulls.listCommits({ ...payload, per_page: 100 });
+      const filtered = res.data
+        .map(({ commit }) => ISSUE_NUMBER_REGEX.exec(commit.message)?.[0])
+        .filter(Boolean) as string[];
+      issueNumbers = [...issueNumbers, ...filtered];
+      if (res.data?.length !== 100) {
+        shouldFetchMore = false;
+      }
+    }
+    setOutput('issueNumbers', issueNumbers.join(','));
   } catch (error: any) {
     console.table(error);
     setFailed(error?.errorMessages);
